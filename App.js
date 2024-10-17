b5a6b20e1ef742e562a20d15a7423a620cbf95f6
Precatsunbeam <yogeshchavan113@gmail.com>
2024-10-17 18:40:31 +0530

    JSX

--- App.js
+++ App.js
@@ -4,82 +4,14 @@ import ReactDOM from "react-dom/client";
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "Hello World From React"
+  "Namaste React 🏆"
 );
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
 
-// console.log(heading);
+//JSX
+const jsxHeading = <h1 id="heading">Namaste React using JSX 🏆</h1>;
 
-/*
-Parent-Child structure
-
-<div id="parent">
-    <div id="child">
-        <h1>I am h1 tag</h1>
-    </div>
-</div>
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I am h1 tag")
-  )
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-*/
-
-/*
-Parent-Child structure with sibling
-
-<div id="parent">
-    <div id="child">
-        <h1>I am h1 tag</h1>
-        <h2>I am h2 tag</h2>
-    </div>
-</div>
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ])
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-*/
-
-/*
-Parent-Child structure with 2 child with sibling
-
-<div id="parent">
-    <div id="child1">
-        <h1>I am h1 tag</h1>
-        <h2>I am h2 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>I am h1 tag</h1>
-        <h2>I am h2 tag</h2>
-    </div>
-</div>
-*/
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(jsxHeading);
