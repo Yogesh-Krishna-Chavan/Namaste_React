46bdd7c239ed7a9e1e037ee39d8de27144dcd71e
Precatsunbeam <yogeshchavan113@gmail.com>
2024-12-01 00:28:18 +0530

    Food Ordering App restro-cart

--- App.js
+++ App.js
@@ -1,38 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
 /*
 #Header
   -Logo
   -Nav Items
 
 #Body
-  -
+  -Search
+  -Restraurant Container
+    -Restro Cards
+      -img
+      -Name of res, start rating, cuisine, deleivery timer
 
 #Footer
-
+  -Copyright
+  -Links
+  -Address
+  -Contact
 */
 
-
-
-
-
-
-
-
-
-
-
-
+/*
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Namaste React 🏆"
 );
-
+ 
 //JSX
-
 const elem = <div>React Element</div>;
 
 const title = (
@@ -71,6 +66,60 @@ const HeadingComponent3 = () => (
     <h1 id="container">Namaste React Functional Component</h1>
   </div>
 );
+*/
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <h3>Shaurya-Wada</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restro-container">
+        {/*Create Restro component for reuse */}
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent3 />);
+root.render(<AppLayout />);
