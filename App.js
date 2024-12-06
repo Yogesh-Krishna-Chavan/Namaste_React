5667f78f2aba9aaab423c396536165125ae40885
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2024-12-06 13:52:58 +0530

    Swiggy App

--- App.js
+++ App.js
@@ -95,6 +95,8 @@ const styleCard = {
 
 const RestaurantCard = (props) => {
   const { resData } = props;
+  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo } =
+    resData?.info;
   return (
     <div className="res-card" style={styleCard}>
       <img
@@ -105,10 +107,10 @@ const RestaurantCard = (props) => {
           resData.info.cloudinaryImageId
         }
       ></img>
-      <h3>{resData.info.name}</h3>
-      <h4>{resData.info.cuisines.join(", ")}</h4>
-      <h4>{resData.info.avgRating}</h4>
-      <h4>{resData.info.costForTwo}</h4>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{costForTwo}</h4>
     </div>
   );
 };
@@ -380,96 +382,6 @@ const resList = [
   },
   {
     info: {
-      id: "945448",
-      name: "Yummy Better",
-      cloudinaryImageId:
-        "RX_THUMBNAIL/IMAGES/VENDOR/2024/8/22/5c6d3a34-1ac1-44ca-94cf-7e9122459ebc_945448.jpg",
-      locality: "Chhindwara City",
-      areaName: "Chhindwara City",
-      costForTwo: "₹200 for two",
-      cuisines: ["South Indian", "Fast Food"],
-      veg: true,
-      parentId: "557138",
-      avgRatingString: "NEW",
-      sla: {
-        deliveryTime: 58,
-        lastMileTravel: 11.2,
-        serviceability: "SERVICEABLE",
-        slaString: "55-60 mins",
-        lastMileTravelString: "11.2 km",
-        iconType: "ICON_TYPE_EMPTY",
-      },
-      availability: {
-        nextCloseTime: "2024-12-07 22:00:00",
-        opened: true,
-      },
-      badges: {
-        imageBadges: [
-          {
-            imageId: "v1695133679/badges/Pure_Veg111.png",
-            description: "pureveg",
-          },
-        ],
-      },
-      isOpen: true,
-      type: "F",
-      badgesV2: {
-        entityBadges: {
-          imageBased: {
-            badgeObject: [
-              {
-                attributes: {
-                  description: "pureveg",
-                  imageId: "v1695133679/badges/Pure_Veg111.png",
-                },
-              },
-            ],
-          },
-          textBased: {},
-          textExtendedBadges: {},
-        },
-      },
-      aggregatedDiscountInfoV3: {
-        header: "10% OFF",
-        subHeader: "UPTO ₹40",
-      },
-      orderabilityCommunication: {
-        title: {},
-        subTitle: {},
-        message: {},
-        customIcon: {},
-      },
-      differentiatedUi: {
-        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-        differentiatedUiMediaDetails: {
-          mediaType: "ADS_MEDIA_ENUM_IMAGE",
-          lottie: {},
-          video: {},
-        },
-      },
-      reviewsSummary: {},
-      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-      isNewlyOnboarded: true,
-      restaurantOfferPresentationInfo: {},
-      externalRatings: {
-        aggregatedRating: {
-          rating: "--",
-        },
-      },
-      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
-    },
-    analytics: {
-      context: "seo-data-1947e390-835f-4bca-8aaf-50c17d66cdce",
-    },
-    cta: {
-      link: "https://www.swiggy.com/city/chhindwara/yummy-better-chhindwara-city-rest945448",
-      text: "RESTAURANT_MENU",
-      type: "WEBLINK",
-    },
-    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
-  },
-  {
-    info: {
       id: "549580",
       name: "Fresh Meal's",
       cloudinaryImageId: "v5e3atn9tmtgcca1onnr",
@@ -726,98 +638,6 @@ const resList = [
     },
     widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
   },
-  {
-    info: {
-      id: "151649",
-      name: "Hotel Sai Nath & Sai Restaurant",
-      cloudinaryImageId: "vkhcohhmqfczycw9vsar",
-      locality: "railway station",
-      areaName: "Chhindwara Locality",
-      costForTwo: "₹200 for two",
-      cuisines: [
-        "North Indian",
-        "South Indian",
-        "Chinese",
-        "Beverages",
-        "Fast Food",
-        "Desserts",
-      ],
-      avgRating: 4.3,
-      veg: true,
-      parentId: "101802",
-      avgRatingString: "4.3",
-      totalRatingsString: "920",
-      sla: {
-        deliveryTime: 51,
-        lastMileTravel: 10.3,
-        serviceability: "SERVICEABLE",
-        slaString: "50-55 mins",
-        lastMileTravelString: "10.3 km",
-        iconType: "ICON_TYPE_EMPTY",
-      },
-      availability: {
-        nextOpenTimeMessage: "Opens next at 11 am, today",
-      },
-      badges: {
-        imageBadges: [
-          {
-            imageId: "v1695133679/badges/Pure_Veg111.png",
-            description: "pureveg",
-          },
-        ],
-      },
-      aggregatedDiscountInfoV2: {},
-      type: "F",
-      badgesV2: {
-        entityBadges: {
-          imageBased: {
-            badgeObject: [
-              {
-                attributes: {
-                  description: "pureveg",
-                  imageId: "v1695133679/badges/Pure_Veg111.png",
-                },
-              },
-            ],
-          },
-          textBased: {},
-          textExtendedBadges: {},
-        },
-      },
-      orderabilityCommunication: {
-        title: {},
-        subTitle: {},
-        message: {},
-        customIcon: {},
-      },
-      differentiatedUi: {
-        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-        differentiatedUiMediaDetails: {
-          mediaType: "ADS_MEDIA_ENUM_IMAGE",
-          lottie: {},
-          video: {},
-        },
-      },
-      reviewsSummary: {},
-      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-      restaurantOfferPresentationInfo: {},
-      externalRatings: {
-        aggregatedRating: {
-          rating: "--",
-        },
-      },
-      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
-    },
-    analytics: {
-      context: "seo-data-1947e390-835f-4bca-8aaf-50c17d66cdce",
-    },
-    cta: {
-      link: "https://www.swiggy.com/city/chhindwara/hotel-sai-nath-and-sai-restaurant-railway-station-chhindwara-locality-rest151649",
-      text: "RESTAURANT_MENU",
-      type: "WEBLINK",
-    },
-    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
-  },
 ];
 
 const Body = () => {
@@ -825,8 +645,9 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="restro-container">
-        {/*Create Restro component for reuse */}
-        <RestaurantCard resData={resList[0]} />
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
