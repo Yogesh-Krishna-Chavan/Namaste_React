5ba75af099b6752cd47b7ff3cdf5cd1e3d2006d5
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2024-12-27 13:40:50 +0530

    Contact Us page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./components/about";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 /*
 const heading = React.createElement(
@@ -65,10 +67,15 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <Error />,
   },
   {
     path: "/about",
     element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
   },
 ]);
 
