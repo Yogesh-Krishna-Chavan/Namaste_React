0a532898974e0a89441eca5d9af00479ca57a687
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2025-01-01 14:59:19 +0530

    Online/Offline

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   //State variable
@@ -40,10 +41,16 @@ const Body = () => {
     );
   };
 
-  //Conditional Rendering
-  if (listOfRestaurants.length === 0) {
-    return <Shimmer />;
-  }
+  // Conditional Rendering
+  // if (listOfRestaurants.length === 0) {
+  //   return <Shimmer />;
+  // }
+
+  const onlineStatus = useOnlineStatus();
+  if (onlineStatus === false)
+    return (
+      <h1>Looks like you are offline...📴 Please check your internet....🛜</h1>
+    );
 
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
